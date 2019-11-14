8201b31d1cfb30a48ed1e7c772edfe30ddaa6484
Seyyed Ali Akhavani <sa.akhavani@gmail.com>
2019-11-14 15:50:33 -0500

    Add deployer Dockerfile and config

---
+++ deployer/user.js
@@ -0,0 +1,6 @@
+user_pref("app.update.auto", false);
+user_pref("devtools.debugger.force-local", false);
+user_pref("devtools.debugger.remote-enabled", true);
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.prompt-connection", false);
+user_pref("browser.dom.window.dump.enabled", true);
