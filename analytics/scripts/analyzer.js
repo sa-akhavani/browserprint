e1724ec5b5ddd70b533d3bea4f19e273be4c5f5e
Seyed Ali AKhavani <sa.akhavani@gmail.com>
2020-02-26 14:37:40 -0500

    Fix feature extractor to extract all leaves of the feature tree

--- analytics/scripts/analyzer.js
+++ analytics/scripts/analyzer.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const reportsDir = ('../reports')
 
+stack = [];
 
 function intersection(setA, setB) {
     let _intersection = new Set();
@@ -22,40 +23,85 @@ function difference(setA, setB) {
     return _difference
 }
 
+function isNameKey(keyName) {
+    if (keyName.includes(".name"))
+        return true;
+    else
+        return false;
+}
+
+async function doreBatel(featuresObject) {
+    for (const featureField in featuresObject) {
+        if (typeof featuresObject[featureField] === 'object') {
+            let feature = featuresObject[featureField];
+            for (let field in feature) {
+                if (isNameKey(field)) {
+                    stack.push(field.replace('.name', ''));
+                } else if (typeof feature[field] === 'object') {
+                    await doreBatel(feature[field])
+                }
+            }
+        } else {
+            if (isNameKey(featureField)) {
+                stack.push(featureField.replace('.name', ''));
+            }
+        }
+    }
+}
+
+async function extractFeatureNames(featuresObject) {
+    stack = [];
+    await doreBatel(featuresObject);
+    let featuresList = stack;
+    // console.dir(featuresList, { maxArrayLength: null });
+    featuresSet = new Set()
+    for(f of featuresList) {
+        featuresSet.add(f);
+    }
+    return featuresSet;
+}
+
 async function parseFile(filePath) {
-    // console.log('parsing file:', filePath)
     fileContentString = fs.readFileSync(filePath, 'utf-8');
     fileContent = JSON.parse(fileContentString)
     featuresList = fileContent.features.globalObject;
-    return featuresNames = Object.keys(featuresList)
+    return featuresList;
 }
 
 async function main() {
-    let reportsList = fs.readdirSync(reportsDir)
+    let browserReportFileList = fs.readdirSync(reportsDir)
     let allFeatures = new Set();
     let uniqueFeatures = new Set();
     let commonFeatures = new Set();
     let uncommonFeatures = new Set();
-    for (let i = 0; i < reportsList.length; i++) {
-        const reportFile = reportsList[i];
-        let featuresNames = await parseFile(reportsDir + '/' + reportFile)
-        for (feature of featuresNames) {
-            if (allFeatures.has(feature)) {
-                uniqueFeatures.delete(feature);
-            } else {
-                allFeatures.add(feature);
-                uniqueFeatures.add(feature);
-            }
-            if (i == 0)
-                commonFeatures.add(feature);
-        }        
-        commonFeatures = intersection(commonFeatures, featuresNames);
-        uncommonFeatures = difference(allFeatures, commonFeatures);
+    let finalReport = [];
+    for (let browserFile of browserReportFileList) {
+        let featuresObject = await parseFile(reportsDir + '/' + browserFile);
+        console.log('Victim: ', browserFile)
+        let featuresList = await extractFeatureNames(featuresObject);
+        let browserFeatureData = {
+            browser: browserFile,
+            featuresList: featuresList
+        };
+        finalReport.push(browserFeatureData);
+
+
+        // console.log(finalReport)
+        // for (feature of featuresNames) {
+        //     if (allFeatures.has(feature)) {
+        //         uniqueFeatures.delete(feature);
+        //     } else {
+        //         allFeatures.add(feature);
+        //         uniqueFeatures.add(feature);
+        //     }
+        //     if (i == 0)
+        //         commonFeatures.add(feature);
+        // }        
+        // commonFeatures = intersection(commonFeatures, featuresNames);
+        // uncommonFeatures = difference(allFeatures, commonFeatures);
     }
-    // console.log(commonFeatures);
-    console.log(uncommonFeatures);
-    // console.log(allFeatures);
-    // console.log(uniqueFeatures);
+    console.log(finalReport)
+    // console.log(uncommonFeatures);
 }
 
 
