a412891154a08d05962913e185b20d392980fd7a
Seyed Ali AKhavani <sa.akhavani@gmail.com>
2020-05-14 13:56:02 -0400

    Add new reports (Jan 2018 - April 2020) and remove old ones

--- analytics/scripts/report-list.js
+++ analytics/scripts/report-list.js
@@ -1,221 +1,199 @@
-[{
+[
+    {
         browser: "chrome",
-        version: "79",
-        uuid: "07c7ce9a-f811-4eac-ae16-a7fbb0c4c5cd",
+        version: "81",
+        uuid: "6ad3d4f1-2a4d-4a59-9d19-8f4c55bd9b18",
+        os: "windows10"
+    },
+    {
+        browser: "chrome",
+        version: "80",
+        uuid: "40da67f3-efeb-4a0a-a3c4-ede98bc41cbb",
         os: "windows10"
     }, {
         browser: "chrome",
-        version: "80",
-        uuid: "ba4217f1-b314-4c74-ad49-351633ba70cd",
+        version: "79",
+        uuid: "a501f161-dfcc-475c-a739-f37a9761d214",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "78",
-        uuid: "91e77e8a-710a-4e7f-a706-7976f867191f",
+        uuid: "5dab2bce-9d04-441f-9600-408fd3cc688d",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "77",
-        uuid: "0fe245d8-ccb3-40ce-863b-11bc39b21671",
+        uuid: "758fb911-731f-4dd8-b8e2-1a5d47345027",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "76",
-        uuid: "3ff80d13-126e-44ea-b915-66388278bde6",
+        uuid: "9ececb7c-0d2e-4962-8a9f-a924b082da97",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "75",
-        uuid: "6c34688e-d602-4afe-942b-3c2c303ba478",
+        uuid: "7c8257a7-4614-44dd-ab6b-1bce20d08979",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "74",
-        uuid: "75286755-0569-46c1-aad1-024a766498ab",
+        uuid: "34045bf7-dc95-4ea9-bd32-d510d951f653",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "73",
-        uuid: "aaed1e6a-1a30-42af-9dcf-fa096a86c1cc",
+        uuid: "846271e2-ca02-48ee-a114-f5bea0929e27",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "72",
-        uuid: "931320c4-f3d8-43ff-9c8a-aa9904361b19",
+        uuid: "302b0b7e-29d6-4717-8560-cefc5e396217",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "71",
-        uuid: "7814851a-c21c-42c0-a5b8-2e72bd57df4c",
+        uuid: "3b83109a-2ce0-40e2-a917-88504f87b2cc",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "70",
-        uuid: "8bfe3e37-90c4-40c8-821b-3fbdff69f5bd",
+        uuid: "89aeb8b0-8bf8-4ea5-9b1b-306e966ba447",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "69",
-        uuid: "e02f3065-6668-4ddd-bc19-ca3808dade89",
+        uuid: "fe78f076-29be-4803-8136-69efff5dffbe",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "68",
-        uuid: "1de0f732-c6d1-4ef7-b165-abecd67838f6",
+        uuid: "39634cba-636d-43e8-b1c1-0b96f0ff5bef",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "67",
-        uuid: "0fe1f80a-30bc-4ad3-b703-22a9abc4014c",
+        uuid: "65f059f7-1f22-4ec4-a714-36d524c33cd6",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "66",
-        uuid: "b45d75e2-a82b-4fae-8b4d-badf7c10c40f",
+        uuid: "dba7b79b-d536-4b40-90a4-e5bdff45db1d",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "65",
-        uuid: "380ff59d-26e1-4034-97a0-2537c10857c4",
+        uuid: "7d8fb3a9-3c71-4e8a-bf0f-0d13ba751e16",
         os: "windows10"
     },
     {
         browser: "chrome",
         version: "64",
-        uuid: "500d59f8-6852-4b2a-9ec0-ed07a231d160",
+        uuid: "6ecb405d-29d4-409b-8dbc-3a535b4ed588",
         os: "windows10"
-    },
-    {
-        browser: "chrome",
+    }, {
+        browser: "firefox",
+        version: "75",
+        uuid: "bf39236b-e930-4023-aa86-ea0072684d1d",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "74",
+        uuid: "f696c467-9b93-4555-ab39-302035635f52",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "73",
+        uuid: "f5a71eb9-b035-44c3-b2cf-4c34b9b948e0",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "72",
+        uuid: "8725366a-10c7-4c23-838c-0c202d3f6e6d",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "71",
+        uuid: "0a9fea6c-9912-4895-b3f9-00e50e4a8af0",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "70",
+        uuid: "8e6e4952-3212-41fc-984e-c1aa8ce21b34",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "69",
+        uuid: "5641d177-9dab-406d-b4f9-7e475e9499e8",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "68",
+        uuid: "630126bb-bacc-42ed-a1f1-09503688a827",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "67",
+        uuid: "94d6131a-f8cb-4f73-9865-0bf0ff4532b7",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "66",
+        uuid: "8838d21f-cff3-4f77-841c-b9f7bf33167c",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "65",
+        uuid: "c3c7194b-1a4c-4e71-90f5-064bd3e43c3f",
+        os: "windows10"
+    }, {
+        browser: "firefox",
+        version: "64",
+        uuid: "6c64b7e4-b7bd-480e-bdbc-1faa6e626a29",
+        os: "windows10"
+    }, {
+        browser: "firefox",
         version: "63",
-        uuid: "2edae6e9-b970-4282-81c8-24c7691b0f63",
+        uuid: "504d70d1-f465-473a-90e7-1729ee86b314",
         os: "windows10"
-    },
-    {
-        browser: "chrome",
+    }, {
+        browser: "firefox",
         version: "62",
-        uuid: "fb8daa94-4f7a-43ba-869b-ec14264e704a",
+        uuid: "3989b693-1740-4c24-b4fd-c99870274aa0",
         os: "windows10"
-    },
-    {
-        browser: "chrome",
+    }, {
+        browser: "firefox",
         version: "61",
-        uuid: "6da8f432-582f-4c16-8885-e5bf40f3b483",
+        uuid: "774b8928-bf1c-4769-b0f5-afb94df1a9a4",
         os: "windows10"
-    },
-    {
-        browser: "chrome",
+    }, {
+        browser: "firefox",
         version: "60",
-        uuid: "17f8c76d-32ad-4e13-902a-6690ffd9a653",
+        uuid: "54724876-d550-425e-a08c-19ef2a2bb6ad",
         os: "windows10"
-    },
-    {
-        browser: "chrome",
+    }, {
+        browser: "firefox",
         version: "59",
-        uuid: "01fbe393-4e83-4701-b9cb-880529f60562",
+        uuid: "ba1a6ae5-a57a-429b-b8b9-f795f5af8943",
         os: "windows10"
-    },
-    {
-        browser: "chrome",
+    }, {
+        browser: "firefox",
         version: "58",
-        uuid: "1219c76a-8456-4a6d-aff0-d22e32d851db",
-        os: "windows10"
-    },
-    {
-        browser: "chrome",
-        version: "57",
-        uuid: "3818498d-15cf-43f0-a251-d784a19d1271",
-        os: "windows10"
-    },
-    {
-        browser: "chrome",
-        version: "56",
-        uuid: "5fa04389-a2aa-43b2-a5b9-abec27b2cb16",
-        os: "windows10"
-    },
-    {
-        browser: "chrome",
-        version: "55",
-        uuid: "1958972e-4cb9-4000-bf71-0e4c91d133e1",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "72",
-        uuid: "33708f15-3d6b-404c-b136-47738088051a",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "71",
-        uuid: "b786e6b9-ec07-465b-8097-04bf13046ba0",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "70",
-        uuid: "3314e8e5-9572-4292-a1d2-baa36877ca09",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "69",
-        uuid: "d1da377b-ff50-4615-84b3-6c5185b0bb87",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "68",
-        uuid: "08f0d089-0761-4ef6-89c9-f0885662d0da",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "67",
-        uuid: "654acdb0-1194-4d39-ae65-265db9af63a9",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "66",
-        uuid: "92af89c8-b0cf-4f12-98d3-99c94f0331e9",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "65",
-        uuid: "b0d6717f-92d8-4491-9df5-82326d5ea350",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "64",
-        uuid: "d741168d-2e8e-4fdc-8433-84ef5881ce18",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "63",
-        uuid: "235fed82-5cbc-438a-8768-3fdd3a46dde3",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "62",
-        uuid: "3034b5bc-8166-4ae9-92b9-85d7ea1119ef",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "61",
-        uuid: "b62a8cd3-e0a4-4400-8674-61a3f1fdeb18",
-        os: "windows10"
-    }, {
-        browser: "firefox",
-        version: "60",
-        uuid: "ec555d1d-5d78-4e67-b016-de7e6afd06aa",
+        uuid: "fb8cb6ff-d7e3-48ec-b00e-e19748b32233",
         os: "windows10"
     }
 ]
