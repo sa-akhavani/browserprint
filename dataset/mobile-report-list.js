8766805db5919d091c65cec6f4a309dad7a81a3f
Seyed Ali AKhavani <sa.akhavani@gmail.com>
2021-06-28 20:05:49 -0400

    add opera and mobile datasets

---
+++ dataset/mobile-report-list.js
@@ -0,0 +1,16 @@
+[{
+    browser: "chrome",
+    version: "86",
+    uuid: "d2502cb8-9dad-466f-b63a-a8b4157006f7",
+    os: "iphone11 pro"
+}, {
+    browser: "chrome",
+    version: "89",
+    uuid: "b88c0e6d-7048-4ccd-bfb8-3d84aa84727b",
+    os: "galaxy s21"
+}, {
+    browser: "firefox",
+    version: "80",
+    uuid: "6b19d893-6834-4a71-8ebe-6517700c4949",
+    os: "galaxy s20"
+}]
