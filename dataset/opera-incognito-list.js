e4559f95a10b9a293e2c9c5601a2d230f080e948
Ali Akhavani <sa.akhavani@gmail.com>
2021-06-29 17:57:14 +0000

    Add opera incognito list

---
+++ dataset/opera-incognito-list.js
@@ -0,0 +1,165 @@
+[
+    {
+        browser: "opera",
+        version: "68",
+        uuid: "849a16dc-a57d-4142-a5c3-00876dd1d58b",
+        os: "windows10"
+    },
+    {
+        browser: "opera",
+        version: "67",
+        uuid: "277c8d95-010b-4826-9f2a-a38286b71de7",
+        os: "windows10"
+    },
+    {
+        browser: "opera",
+        version: "66",
+        uuid: "1a5689b0-9d3f-4310-b9f6-f9cc45c827ef",
+        os: "windows10"
+    },
+    {
+        browser: "opera",
+        version: "65",
+        uuid: "428525fb-dc24-45c9-a858-8be4cb6897a8",
+        os: "windows10"
+    },
+    {
+        browser: "opera",
+        version: "64",
+        uuid: "95b85b68-ea8b-4fe2-a10f-d63709b8a627",
+        os: "windows10"
+    },
+    {
+        browser: "opera",
+        version: "63",
+        uuid: "3119fbb8-9812-4450-9ba7-f7d0ad59de73",
+        os: "windows10"
+    },
+    {
+        browser: "opera",
+        version: "62",
+        uuid: "7f4e94e2-6592-4105-b51e-5ddb54e48a23",
+        os: "windows10"
+    },
+    {
+        browser: "opera",
+        version: "60",
+        uuid: "378471b8-9b7c-42e7-b997-7d5842d8e852",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "58",
+        uuid: "deb14b4b-1c4f-4a33-aba9-82654e9ef11a",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "57",
+        uuid: "ecdd4fd3-a878-4c1a-a39e-d09edf4043a5",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "56",
+        uuid: "6ca6da3b-7353-4739-bf91-0aac2d090152",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "55",
+        uuid: "723c2598-f8ba-4200-a9db-f44c955e3e69",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "54",
+        uuid: "cfe2ab91-051a-4a42-ab01-b6bddbc21999",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "53",
+        uuid: "ac5b2421-2567-4eda-8847-b35c82a0c7ce",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "52",
+        uuid: "065bd3af-a2b3-4da9-85a1-3f17f1408c17",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "51",
+        uuid: "6ae8452c-a465-4360-a816-603234221ebd",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "50",
+        uuid: "3b9196a2-a7a6-4c7a-b807-d12f3a0854c0",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "49",
+        uuid: "fa886bb1-4e6d-40ba-b977-87a9ef6034e3",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "48",
+        uuid: "1c0622d9-8c4d-4c7f-9235-545824d4994a",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "47",
+        uuid: "6de8f23c-b0ba-4e4d-9cd7-2f5dabec31af",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "46",
+        uuid: "e8b1e5cc-ccf5-4cbf-a672-367f4c6a9848",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "45",
+        uuid: "67d14777-7bbe-4bcd-8358-bf74ea36ed17",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "44",
+        uuid: "c7f118a7-0d8e-492b-ab00-3e456718a348",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "43",
+        uuid: "c3c79fea-8f5f-4e4f-8d82-14d00b365938",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "42",
+        uuid: "78ee3141-7fff-430e-b8bf-107cc0d37f30",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "41",
+        uuid: "db8da0a1-bc9f-4369-8f0a-b86b5da8aecd",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "40",
+        uuid: "623155b1-b120-42b8-b640-65648c71ec63",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "39",
+        uuid: "5a0f9405-2826-42ab-a517-37a2e1fb2f57",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "38",
+        uuid: "247690ca-0dae-48aa-94b4-dcd76a361143",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "37",
+        uuid: "cfa86e27-dcca-470f-9aa9-5f4411c14e61",
+        os: "windows10"
+    }, {
+        browser: "opera",
+        version: "36",
+        uuid: "4e697e8b-ca7f-4d32-a5b3-7524882abfd8",
+        os: "windows10"
+    }
+]
